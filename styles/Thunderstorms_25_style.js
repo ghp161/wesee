d6311e13a724badbb05f5c2a7a2d20d56aeb2757
ghp161 <ghp161@gmail.com>
2024-09-02 10:54:06 +0530

    Add files via upload

---
+++ styles/Thunderstorms_25_style.js
@@ -0,0 +1,39 @@
+var size = 0;
+var placement = 'point';
+
+var style_Thunderstorms_25 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [313, 313],
+                  scale: 0.04792332268370607,
+                  anchor: [7, 7],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/thunderstorm.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
